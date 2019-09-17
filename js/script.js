ed37480a0b505391e00718a5fe9e705f54aa56c3
Oleg <gvozdyk23@gmail.com>
2019-09-17 13:39:23 +0300

    mob v.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-var swiper = new Swiper('.swiper-container', {
+let burger = document.querySelector('.burger'),
+    menu = document.querySelector('.nav_menu');
+
+burger.addEventListener('click', function() {
+   menu.classList.toggle('menu_active');
+});
+
+let swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
